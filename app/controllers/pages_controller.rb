0a35d355c74df6aec5a464afcab9f2c8a905a6d9
Jan-Jelles van Stormbroek <bullace@gmail.com>
2015-05-12 11:52:14 +0200

    Added seperate home page for admins and students

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 
   def home
+    if current_user.role == "superadmin" || current_user.role == "admin"
+      render "pages/home_admin"
+    else
+      render "pages/home_student"
+    end
   end
 
 end
