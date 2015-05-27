4099ed3750759b1df0f4177b3cf7ec2fc738ec91
Erwin <erwin058@gmail.com>
2015-05-27 13:56:21 +0200

    Fixed menu and sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
 $( document ).ready(function() {
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+	$("#menu-toggle").click(function(e) {
+	    e.preventDefault();
+	    $("#wrapper").toggleClass("toggled");
+	});
 });
