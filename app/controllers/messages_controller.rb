416e76680df1694077f3aed6d1644825db0a24b8
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-05-27 20:19:05 +0200

    Changed if statement for curren_user role check from enum to string value

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
   end
 
   def inbox
-    if current_user.role = 1
+    if current_user.role = "user"
       @messages = Message.where.not(sender_id: current_user).joins(:scenario_session).where(:scenario_sessions => { student_id: current_user } ).all
     else
       @messages = Message.where.not(sender_id: current_user).joins(:scenario_session).where(:scenario_sessions => { teacher_id: current_user } ).all
