94dae33c58c9eefd11f0f0d9a5152b6dad49fd6c
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-05-28 12:19:04 +0200

    Changing menu javascript so it works with turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-$( document ).ready(function() {
-	$("#menu-toggle").click(function(e) {
-	    e.preventDefault();
-	    $("#wrapper").toggleClass("toggled");
-	});
-});
