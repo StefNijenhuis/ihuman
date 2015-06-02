522a53fd21a7d5761c6caab0f5af62f1ffbab0e8
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-06-02 12:03:59 +0200

    Updating scenario controller

--- app/controllers/admin/scenarios_controller.rb
+++ app/controllers/admin/scenarios_controller.rb
@@ -1,11 +1,19 @@
 class Admin::ScenariosController < ApplicationController
   #after_action :verify_authorized
 
-    def index
+  def index
     @scenarios = Scenario.all
   end
 
   def new
+    @scenario = Scenario.new
+  end
+
+  def create
+    @scenario = Scenario.new(scenario_params)
+    if @scenario.save
+      redirect_to @scenario
+    end
   end
 
 end
