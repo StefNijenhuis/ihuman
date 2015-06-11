5ecb6c6ed5d0aeec95701d8c8a41cafa32b7b371
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-06-11 10:38:07 +0200

    Created ajxa saving method

--- app/controllers/admin/scenarios_controller.rb
+++ app/controllers/admin/scenarios_controller.rb
@@ -9,19 +9,26 @@ class Admin::ScenariosController < ApplicationController
     @scenario = Scenario.new
   end
 
-  def create
-    @scenario = Scenario.new(scenario_params)
-    # @scenario.content =
-    if @scenario.save
-      redirect_to @scenario
-    end
-  end
+  # def create
+  #   @scenario = Scenario.new(scenario_params)
+  #   if @scenario.save
+  #     redirect_to @scenario
+  #   end
+  # end
 
   def ajax_save
     json = request.POST['data']
     obj = JSON.parse json
     title = obj["title"]
     # obj["scenario"]['briefing']['content']
+    @scenario = Scenario.new
+    @scenario.title = title
+    @scenario.content = json
+    if @scenario.save
+      render :json => {status: "success"}
+    else
+      render :json => {status: "fail"}
+    end
 
     render :json => {success:"Joy"}
   end
