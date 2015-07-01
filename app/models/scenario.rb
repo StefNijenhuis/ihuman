1638c72aa4dd300eec92f4f1079e962946458cb6
Jan-Jelles van Stormbroek <bullace@gmail.com>
2015-07-01 16:54:47 +0200

    Adding node function that returns scenario objects on id

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,4 +2,33 @@ class Scenario < ActiveRecord::Base
 
   has_many :scenario_session
 
+  def node(id = 0, obj = self)
+    object = nil
+
+    if obj == self
+      obj = JSON.parse self.content
+      obj = obj["scenario"]['briefing']
+    end
+
+    if id == 0
+      return obj
+    else
+      obj['children'].each do |child|
+        if child["id"] == id
+          object = child
+          break
+        end
+
+        if child['children'].present?
+          object = self.node(id, child)
+          if object.present?
+            break
+          end
+        end
+      end
+    end
+
+    return object
+  end
+
 end
